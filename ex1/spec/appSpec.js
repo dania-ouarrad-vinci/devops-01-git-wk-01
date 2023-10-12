1dcfc95e964aae415aaa423baea8292f7da2f0de
dania <dania.ouarrad@student.vinci.be>
2023-10-12 19:14:16 +0200

    add of ex1

---
+++ ex1/spec/appSpec.js
@@ -0,0 +1,12 @@
+import { Main } from "../src/app.js";
+
+describe("the first minute line", function () {
+  let main = new Main();
+
+  it("function should return ", function () {
+    let result = main.BerlinClock();
+    expect(result).toBe();
+  });
+});
+
+// allumée ou éteinte
