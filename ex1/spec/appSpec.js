1a0a24b83b6017461879d9a1283ec80a1caed203
dania <dania.ouarrad@student.vinci.be>
2023-10-15 11:41:16 +0200

    before refactor

--- ex1/spec/appSpec.js
+++ ex1/spec/appSpec.js
@@ -16,6 +16,35 @@ describe("the first minute line", function () {
     expect(result).toBe("1er lampe jaune")
   })
 
+  it("simpleMinuteLine should return 1er lampe jaune, 2ème lampe jaune when given 0:02",function(){
+    let time = "0:02";
+    let result=berlinClock.simpleMinuteLine(time);
+    expect(result).toBe("1er lampe jaune, 2ème lampe jaune")
+  })
+
+
+  it("simpleMinuteLine should return 1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune when given 0:03",function(){
+    let time = "0:03";
+    let result=berlinClock.simpleMinuteLine(time);
+    expect(result).toBe("1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune")
+  })
+
+  it("simpleMinuteLine should return 1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune, 4ème lampe jaune jaune when given 0:04",function(){
+    let time = "0:04";
+    let result=berlinClock.simpleMinuteLine(time);
+    expect(result).toBe("1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune, 4ème lampe jaune")
+  })
+
+
+
+
+});
+
+
+
+describe("the seconde minute line", function () {
+  let berlinClock = new BerlinClock();
+
 
 });
 
--- ex1/src/app.js
+++ ex1/src/app.js
@@ -1,6 +1,13 @@
 export class BerlinClock {
   simpleMinuteLine(time) {
+    
     if (time === "0:00") return "Aucune lampe allumée";
-    if(time==="0:01") return "1er lampe jaune";
+    if (time === "0:01") return "1er lampe jaune";
+    if (time === "0:02") return "1er lampe jaune, 2ème lampe jaune";
+    if (time === "0:03")return "1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune";
+    if (time === "0:04")return "1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune, 4ème lampe jaune";
   }
+
+
+
 }
