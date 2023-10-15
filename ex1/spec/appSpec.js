6cc5687b13e8baf9ad8c06950a13e17bc3c137ce
dania <dania.ouarrad@student.vinci.be>
2023-10-15 12:38:48 +0200

    add of blocOf5MinuteLine

--- ex1/spec/appSpec.js
+++ ex1/spec/appSpec.js
@@ -9,43 +9,144 @@ describe("the first minute line", function () {
     expect(result).toBe("Aucune lampe allumée");
   });
 
-
-  it("simpleMinuteLine should return 1er lampe jaune when given 0:01",function(){
+  it("simpleMinuteLine should return 1er lampe jaune when given 0:01", function () {
     let time = "0:01";
-    let result=berlinClock.simpleMinuteLine(time);
-    expect(result).toBe("1er lampe jaune")
-  })
+    let result = berlinClock.simpleMinuteLine(time);
+    expect(result).toBe("1er lampe jaune");
+  });
 
-  it("simpleMinuteLine should return 1er lampe jaune, 2ème lampe jaune when given 0:02",function(){
+  it("simpleMinuteLine should return 1er lampe jaune, 2ème lampe jaune when given 0:02", function () {
     let time = "0:02";
-    let result=berlinClock.simpleMinuteLine(time);
-    expect(result).toBe("1er lampe jaune, 2ème lampe jaune")
-  })
+    let result = berlinClock.simpleMinuteLine(time);
+    expect(result).toBe("1er lampe jaune, 2ème lampe jaune");
+  });
 
-
-  it("simpleMinuteLine should return 1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune when given 0:03",function(){
+  it("simpleMinuteLine should return 1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune when given 0:03", function () {
     let time = "0:03";
-    let result=berlinClock.simpleMinuteLine(time);
-    expect(result).toBe("1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune")
-  })
+    let result = berlinClock.simpleMinuteLine(time);
+    expect(result).toBe("1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune");
+  });
 
-  it("simpleMinuteLine should return 1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune, 4ème lampe jaune jaune when given 0:04",function(){
+  it("simpleMinuteLine should return 1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune, 4ème lampe jaune jaune when given 0:04", function () {
     let time = "0:04";
-    let result=berlinClock.simpleMinuteLine(time);
-    expect(result).toBe("1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune, 4ème lampe jaune")
-  })
+    let result = berlinClock.simpleMinuteLine(time);
+    expect(result).toBe(
+      "1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune, 4ème lampe jaune"
+    );
+  });
 
-
-
-
+  it("simpleMinuteLine should return Aucune lampe allumée when given 0:25", function () {
+    let time = "0:25";
+    let result = berlinClock.simpleMinuteLine(time);
+    expect(result).toBe("Aucune lampe allumée");
+  });
 });
-
-
 
 describe("the seconde minute line", function () {
   let berlinClock = new BerlinClock();
+  it("blocOf5MinuteLine should return Aucune lampe allumée when given 0:00 ", function () {
+    let time = "0:00";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe("Aucune lampe allumée");
+  });
 
+  it("blocOf5MinuteLine should return 1er lampe jaune allumée when given 0:05 ", function () {
+    let time = "0:05";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe("1er lampe jaune");
+  });
 
+  it("blocOf5MinuteLine should return 1er lampe jaune allumée when given 0:10 ", function () {
+    let time = "0:10";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe("1er lampe jaune, 2ème lampe jaune");
+  });
+
+  it("blocOf5MinuteLine should return 1er lampe jaune allumée when given 0:15 ", function () {
+    let time = "0:15";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe("1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge");
+  });
+
+  it("blocOf5MinuteLine should return 1er lampe jaune allumée when given 0:20 ", function () {
+    let time = "0:20";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe(
+      "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune"
+    );
+  });
+  it("blocOf5MinuteLine should return 1er lampe jaune allumée when given 0:25 ", function () {
+    let time = "0:25";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe(
+      "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune"
+    );
+  });
+
+  it("blocOf5MinuteLine should return 1er lampe jaune allumée when given 0:35 ", function () {
+    let time = "0:30";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe(
+      "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge"
+    );
+  });
+  it("blocOf5MinuteLine should return 1er lampe jaune allumée when given 0:35 ", function () {
+    let time = "0:35";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe(
+      "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge, 7ème lampe jaune"
+    );
+  });
+  it("blocOf5MinuteLine should return 1er lampe jaune allumée when given 0:40 ", function () {
+    let time = "0:40";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe(
+      "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge, 7ème lampe jaune, 8ème lampe jaune"
+    );
+  });
+  it("blocOf5MinuteLine should return 1er lampe jaune allumée when given 0:45 ", function () {
+    let time = "0:45";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe(
+      "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge, 7ème lampe jaune, 8ème lampe jaune, 9ème lampe rouge"
+    );
+  });
+  it("blocOf5MinuteLine should return 1er lampe jaune allumée when given 0:5£0 ", function () {
+    let time = "0:50";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe(
+      "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge, 7ème lampe jaune, 8ème lampe jaune, 9ème lampe rouge, 10ème lampe jaune"
+    );
+  });
+  it("blocOf5MinuteLine should return 1er lampe jaune allumée when given 0:55 ", function () {
+    let time = "0:55";
+    let result = berlinClock.blocOf5MinuteLine(time);
+    expect(result).toBe(
+      "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge, 7ème lampe jaune, 8ème lampe jaune, 9ème lampe rouge, 10ème lampe jaune, 11ème lampe jaune"
+    );
+  });
+
+  it("blocOf5MinuteLine should return 1er lampe jaune, 2ème lampe jaune 1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune when given 0:13", function () {
+    let time = "0:13";
+    let result =
+      berlinClock.blocOf5MinuteLine(time) +
+      "\n" +
+      berlinClock.simpleMinuteLine(time);
+    expect(result).toBe(
+      "1er lampe jaune, 2ème lampe jaune" +
+        "\n" +
+        "1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune"
+    );
+  });
 });
 
+describe("the first minute line", function () {
+  let berlinClock = new BerlinClock();
+
+  it("simpleHouresLine should return 1er lampe rouge when given 01:00", function () {
+    let time = "01:00";
+    let result = berlinClock.simpleHouresLine(time);
+    expect(result).toBe("1er lampe rouge");
+  });
+});
 // allumée ou éteinte
--- ex1/src/app.js
+++ ex1/src/app.js
@@ -1,13 +1,46 @@
 export class BerlinClock {
   simpleMinuteLine(time) {
-    
-    if (time === "0:00") return "Aucune lampe allumée";
-    if (time === "0:01") return "1er lampe jaune";
-    if (time === "0:02") return "1er lampe jaune, 2ème lampe jaune";
-    if (time === "0:03")return "1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune";
-    if (time === "0:04")return "1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune, 4ème lampe jaune";
+    let minutes = parseInt(time.split(":")[1]);
+
+    if (minutes % 10 === 0 || minutes % 10 === 5) return "Aucune lampe allumée";
+    if (minutes % 10 === 1 || minutes % 10 === 6) return "1er lampe jaune";
+    if (minutes % 10 === 2 || minutes % 10 === 7)
+      return "1er lampe jaune, 2ème lampe jaune";
+    if (minutes % 10 === 3 || minutes % 10 === 8)
+      return "1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune";
+    if (minutes % 10 === 4 || minutes % 10 === 9)
+      return "1er lampe jaune, 2ème lampe jaune, 3ème lampe jaune, 4ème lampe jaune";
   }
 
+  blocOf5MinuteLine(time) {
+    let minutes = parseInt(time.split(":")[1]);
 
+    if (minutes < 5) return "Aucune lampe allumée";
+    if (minutes >= 5 && minutes < 10) return "1er lampe jaune";
+    if (minutes >= 10 && minutes < 15)
+      return "1er lampe jaune, 2ème lampe jaune";
+    if (minutes >= 15 && minutes < 20)
+      return "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge";
+    if (minutes >= 20 && minutes < 25)
+      return "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune";
+    if (minutes >= 25 && minutes < 30)
+      return "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune";
+    if (minutes >= 30 && minutes < 35)
+      return "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge";
+    if (minutes >= 35 && minutes < 40)
+      return "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge, 7ème lampe jaune";
+    if (minutes >= 40 && minutes < 45)
+      return "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge, 7ème lampe jaune, 8ème lampe jaune";
+    if (minutes >= 45 && minutes < 50)
+      return "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge, 7ème lampe jaune, 8ème lampe jaune, 9ème lampe rouge";
+    if (minutes >= 50 && minutes < 55)
+      return "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge, 7ème lampe jaune, 8ème lampe jaune, 9ème lampe rouge, 10ème lampe jaune";
+    if (minutes >= 55 && minutes < 60)
+      return "1er lampe jaune, 2ème lampe jaune, 3ème lampe rouge, 4ème lampe jaune, 5ème lampe jaune, 6ème lampe rouge, 7ème lampe jaune, 8ème lampe jaune, 9ème lampe rouge, 10ème lampe jaune, 11ème lampe jaune";
+  }
 
+  simpleHouresLine(time) {
+    let houres = parseInt(time.split(":")[0]);
+    if (houres % 10 === 1 || houres % 10 === 6) return "1er lampe rouge";
+  }
 }
